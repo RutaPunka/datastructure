cf33df70f728bc4b99630fa41356d7842ee48b26
Ruta <punkaruta@gmail.com>
2018-11-25 10:05:09 +0200

    my first commit in first branch

--- src/com.punka.data/FirstClass.java
+++ src/com.punka.data/FirstClass.java
@@ -2,4 +2,16 @@ package com.punka.data;
 
 public class FirstClass {
     private String name;
+
+    public FirstClass(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
